af15eb666f66c9d076b691bc3ada104bc9a09c8e
jouureee <jouureee@gmail.com>
2021-12-18 16:13:27 +0900

    Update combine publisher

--- First-Combine/Controllers/NotificationViewController.swift
+++ First-Combine/Controllers/NotificationViewController.swift
@@ -7,17 +7,18 @@
 
 import Foundation
 import UIKit
+import Combine
 
 class NotificationViewController : UIViewController {
-    var notificationToken: NSObjectProtocol?
+    
+    var cancellable: Cancellable?
     
     override func viewDidLoad() {
         super.viewDidLoad()
-        notificationToken = NotificationCenter.default.addObserver(forName: UIDevice.orientationDidChangeNotification, object: nil, queue: nil) { _ in
-            if UIDevice.current.orientation == .portrait {
-                print("orientation changed to portrait")
-            }
-        }
+        cancellable = NotificationCenter.default
+            .publisher(for: UIDevice.orientationDidChangeNotification)
+            .filter() { _ in UIDevice.current.orientation == .portrait }
+            .sink() { _ in print ("Orientation changed to portrait.") }
     }
     
     override func viewWillTransition(to size: CGSize, with coordinator: UIViewControllerTransitionCoordinator) {
